329cbb9c1589e1ac910fdc3b35283a855c9c031a
Camilo <camilocanclini@gmail.com>
2022-06-04 22:09:11 -0300

    Se agregó la funcion para registrar usuarios(con metodo json y controles de archivos) y se agregó la funcion de la API para buscar peliculas. Para esto ultimo hay que modificar unas cosas...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-hola hola hola hola hola hola hola hola hola hola hola hola hola hola
+
 __________________________________________________________________________________
 Sistema Cine (Agenda que guarde peliculas en cartelera)
 con ticket (Compra candybar y entrada)- [en caso de que quede corto, le 
@@ -12,28 +12,70 @@ Para que la URL funcione necesitamos pasar este paremetro
 __________________________________________________________________________________
 Developers: Agustin Antunez - Canclini Camilo
 __________________________________________________________________________________
-sadasdadasdsadas
-asdasdasd
+
 
 
 """
-def movieSearch(search):
-    #Aca iria el codigo de la API, agrego algo para que la funcion no me tire error
-    search = "batman"
-    return search
+import json, random
+from urllib.request import urlopen
+
+def movieSearch():
+    url = 'http://www.omdbapi.com/?apikey=71066fc3&s='
+    movieName = input("Ingrese el nombre de la pelicula \n-> ")
+    #movieYear = input("Ingrese el año de la pelicula \n-> ")
+    url += movieName # Se suma el nombre de la pelicula a la URL de la API
+    response = urlopen(url) # Se hace la petición a la API y se guarda la respuesta en una variable
+    data = json.loads(response.read()) # Se hace la conversion de la respuesta a Json
+    #print(data)
+    j=1
+    for i in data["Search"]:
+        print(j,")",i["Title"])
+        j += 1
 def seatSearch():
      ("Aqui esta la matriz")
     #Hay que hacer una matriz o arreglo que contenga los lugares ocupados y disponibles
     #Deberiamos permitir que seleccionen solo los lugares disponibles
     #Para selecionarlos podemos hacer como si fuera un batalla naval con letras y numeros de posicion
 def purchaseResume():
-    print ("Informacion del ticket")
-    #Tenemos que armar un resumen de lo que se pidio
+    print("")
 def ticketPrint(movieName, seatPlace):
     #Aca deberia salir cada ticket comprado, o sea por persona
     #Necesitamos el lugar y nombre de la peli
     #Deberiamos añadir un generador de codigo, para que no se pueda falcificar
     print("Aqui esta el ticket final")
+def templateJsonFile():# Esta funcion limpia el JSON y crea el usuario admin
+    templateJsonUsers = {
+        0:{
+            "mail": "admin",
+            "password": "admin"}
+        } 
+    jsonString = json.dumps(templateJsonUsers)
+    jsonFile = open("cuentas.json", "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+def registerAccount(email, password):
+    idAccount="1"
+    while True:
+        for i in range(0,4):
+            idAccount += str(random.randint(1,9))
+            #print(idAccount)
+        jsonFile = open('cuentas.json')
+        usersRegistered = json.load(jsonFile)
+        if usersRegistered.get(idAccount) == None:
+            usersRegistered [idAccount] = {
+                "mail": email,
+                "password": password
+                }
+            #print (usersRegistered)
+            jsonString = json.dumps(usersRegistered)
+            jsonFile = open("cuentas.json", "w")
+            jsonFile.write(jsonString)
+            jsonFile.close()
+            print (usersRegistered)
+            break
+        else:
+            print ("el id ya existe")     
+
 while True:
     print ("---Bienvenido al Sistema de Cine---")
     print ("-Elija una opción a continuación---")
@@ -42,19 +84,16 @@ while True:
     print ("3) Salir")
     optionSelected = int(input("-> "))
     if optionSelected==1:
-        movieName = input("Ingrese el nombre de la pelicula \n-> ")
-        #movieYear = input("Ingrese el año de la pelicula \n-> ")
-        movieSelected, error = movieSearch(movieName)
-        if movieSearch(error) == 1:
-            print ("La pelicula no fue encontrada")
-            continue #vuelve a las opciones
+        movieSearch()
+        #if movieSearch(error) == 1:
+            #print ("La pelicula no fue encontrada")
+            #continue #vuelve a las opciones
         numberTickets = int(input("Ingrese cantidad de tickets \n-> "))
         if (numberTickets>10):
             print ("Usted a requisado demasiados tickets (Maximo 10) ")
             continue #vuelve a las opciones
         print ("A continuacion se le presentara matriz con los lugares disponibles")
         seatSearch()
-        placeSelected = int(input("Ingrese los lugares \n-> "))
         print ("Esta informacion es correcta")
         purchaseResume()
         confirmation = input("Ingrese Y para confirmar, N para cancelar")
@@ -66,9 +105,6 @@ while True:
             print ("Volviendo al menu")
             a=input("")
             continue
-    if optionSelected == 2:
-        print ("hola")
-        continue
     if optionSelected ==3:
         print ("Finalizacion del programa")
         break
